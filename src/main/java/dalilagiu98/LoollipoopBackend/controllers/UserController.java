028dcbd51741c652547eea2defb750d970f87d7a
dalilagiu98 <148708941+dalilagiu98@users.noreply.github.com>
2024-05-04 20:55:43 +0200

    created new endpoints for authenticated user

--- src/main/java/dalilagiu98/LoollipoopBackend/controllers/UserController.java
+++ src/main/java/dalilagiu98/LoollipoopBackend/controllers/UserController.java
@@ -7,6 +7,8 @@ import dalilagiu98.LoollipoopBackend.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +21,7 @@ public class UserController {
 
 
     @GetMapping
+    @PreAuthorize("hasAnyAuthority('[GUEST, HOST]')")
     public Page<User> getAllUsers(@RequestParam (defaultValue = "0") int page,
                                   @RequestParam(defaultValue = "10") int size,
                                   @RequestParam(defaultValue = "id") String sortBy){
@@ -26,20 +29,37 @@ public class UserController {
     }
 
     @GetMapping("/{userId}")
-    public User findById(@PathVariable long id){
-        return this.userService.findById(id);
+    @PreAuthorize("hasAnyAuthority('[GUEST]')")
+    public User findById(@PathVariable long userId){
+        return this.userService.findById(userId);
     }
 
 
     @PutMapping("/{userId}")
-    public User updateById(@PathVariable long id, @RequestBody @Validated NewUserRequestDTO body, BindingResult validation){
+    public User updateById(@PathVariable long userId, @RequestBody @Validated NewUserRequestDTO body, BindingResult validation){
         if (validation.hasErrors()) throw new BadRequestException(validation.getAllErrors());
-        else return userService.update(id, body);
+        else return userService.update(userId, body);
     }
 
     @DeleteMapping("/{userId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteUser(@PathVariable long id){
-        this.userService.delete(id);
+    public void deleteUser(@PathVariable long userId){
+        this.userService.delete(userId);
+    }
+
+    @GetMapping("/me")
+    public User getProfile(@AuthenticationPrincipal User currentAuthenticatedUser){
+        return currentAuthenticatedUser;
+    }
+
+    @PutMapping("/me")
+    public User updateProfile(@AuthenticationPrincipal User currentAuthenticatedUser, @RequestBody NewUserRequestDTO updateUser){
+        return this.userService.update(currentAuthenticatedUser.getId(), updateUser);
+    }
+
+    @DeleteMapping("/me")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteProfile(@AuthenticationPrincipal User currentAuthenticatedUser){
+        this.userService.delete(currentAuthenticatedUser.getId());
     }
 }
