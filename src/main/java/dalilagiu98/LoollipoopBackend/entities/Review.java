f13fc8a59deb22f7c4d67f5319904305d2e26ca8
dalilagiu98 <148708941+dalilagiu98@users.noreply.github.com>
2024-05-06 23:07:26 +0200

    creating constructor

--- src/main/java/dalilagiu98/LoollipoopBackend/entities/Review.java
+++ src/main/java/dalilagiu98/LoollipoopBackend/entities/Review.java
@@ -1,20 +1,19 @@
 package dalilagiu98.LoollipoopBackend.entities;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
 public abstract class Review {
     //ATTRIBUTES LIST:
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
     private long id;
     private int score;
     private String description;
